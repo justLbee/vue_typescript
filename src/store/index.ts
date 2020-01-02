393a2b347ed6a20cb2f7d2ec91e5c36bce5b0202
Dmitry Tsygankov <dmitry.u.ts@gmail.com>
2020-01-02 17:58:56 +0300

    typescript vuetify

---
+++ src/store/index.ts
@@ -0,0 +1,14 @@
+import Vue                  from 'vue';
+import Vuex, {StoreOptions} from 'vuex';
+import {RootState}          from './types';
+
+Vue.use(Vuex);
+
+const store: StoreOptions<RootState> = {
+  state: {
+    version: '1.0.0' // a simple property
+  },
+  modules: {}
+};
+
+export default new Vuex.Store<RootState>(store);
